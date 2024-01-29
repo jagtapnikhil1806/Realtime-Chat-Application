c109f00029fe5158291468138c78f6ed79d66ee7
NikhilJagtap1806 <jagtapnikhil1806@gmail.com>
2024-01-29 23:44:42 +0530

    nodejs code completed

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express=require('express');
 const cors=require('cors');
+const axios=require('axios')
 
 // const PORT=3000;
 
@@ -10,10 +11,23 @@ app.use(cors({origin:true}));
 
 app.post("/authenticate", async (req, res) => {
     const { username } = req.body;
-    return res.json({ username: username, secret: "sha256..." });
+
+try{
+const r = await axios.put('https://api.chatengine.io/users/',{
+    username:username,secret:username,first_name:username},{
+        headers:{"private-key":"43c62ede-65c5-44de-8f39-53b6378b08cd"}})
+return res.status(r.status).json(r.data)
+}
+
+catch (e){
+    return res.status(e.response.status).json(e.response.data);
+
+
+}
+    
   });
 
-app.listen(3001)
+app.listen(3000)
 // ,()=>{
 //     console.log(`server is running at http://localhost:${PORT}`)
 // })
